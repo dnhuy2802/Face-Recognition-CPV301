fcbc934879a8bcb1ca3bf6a2cc90206d9be106dc
Ming-doan <quangminh57dng@gmail.com>
2023-07-06 01:37:07 +0700

    new: Capture APIs - Upload Images - Fetch All Faces - Delete Face - Change folder resource to cache

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
+from ..models import IdentityModel, ResponseObject
+from ..utils.save_images import save_images, resize_images
+from ..utils.get_identifiers import get_identifiers
+from ..utils.delete_identifier import delete_identifier
 
 
 ### Create Blueprint ###
@@ -6,6 +10,31 @@ upload_bp = Blueprint('upload', __name__, url_prefix='/upload')
 
 
 ### Routes ###
-@upload_bp.route('/', methods=['GET'])
+@upload_bp.route('/images', methods=['POST'])
 def upload():
-    return "Upload Image"
+    # Get the image from the request
+    person = IdentityModel.from_json(request.get_json())
+    # Save the image to resource folder
+    path = save_images(person.name, person.images)
+    # Resize the images
+    resize_images(path)
+    # Return the id of the person
+    return ResponseObject.success(person.id)
+
+
+@upload_bp.route('/identifiers', methods=['GET'])
+def identifiers():
+    identifiers = []
+    for identifier in get_identifiers():
+        identifiers.append({
+            'id': identifier.id,
+            'name': identifier.name,
+            'thumbnail': identifier.images[0]
+        })
+    return ResponseObject.success(identifiers)
+
+
+@upload_bp.route('/delete/<id>', methods=['DELETE'])
+def delete(id):
+    delete_identifier(id)
+    return ResponseObject.success(True)
---
+++ app/utils/constants.py
@@ -0,0 +1,5 @@
+### App Constants ###
+import os
+
+CACHE_DIR = os.path.join(os.getcwd(), 'app', 'cache')
+IMAGE_SIZE = (112, 112)
